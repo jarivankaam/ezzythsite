7ba5baba61280aa24483333f54bfc823a4b787f0
MarijnvSprundel <71122319+MarijnvSprundel@users.noreply.github.com>
2021-10-06 22:22:35 +0200

    Website javascript and server side javascript updates

--- data/index.js
+++ data/index.js
@@ -1,48 +1,62 @@
 const puppeteer = require('puppeteer');
-let fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 const { stringify } = require('querystring');
+getTitleJson();
 
-(async () => {
-    let browser = await puppeteer.launch({headless: false});
-    let page = await browser.newPage();
-    await page.goto('https://www.youtube.com/c/OfficialDuckStudios/videos/');
-    await page.click('button[aria-label="Agree to the use of cookies and other data for the purposes described"]');
-    await page.waitForNavigation();
-    const data = await page.evaluate(async () => {
-        return await new Promise((resolve, reject) =>{
-            try{
-                let scrollCheck = [];
-                let scrollCheckLast = [];
-                let intervaller = setInterval(() => {
-                    scrollCheck.push(window.scrollY);
-                    scrollCheckLast = scrollCheck.slice(-6);
-                    if(scrollCheckLast.every( (val, i, arr) => val === arr[0] ) && scrollCheckLast[0] != 0){
-                        letdata = Array.from(document.querySelectorAll('#video-title'), element => [element.textContent, element.href]);
-                        clearInterval(intervaller);
-                        resolve(letdata);
-                    }
-                    window.scrollBy(0, 1920);
-                }, 500);
+const requestListener = async function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  console.log("Connected!");
+  res.end(fs.readFileSync('duck.json', 'utf8'));
+}
 
-            }
-            catch(err){
-                console.log(err);
-                reject(err.toString());
-            }
-        });
+const server = http.createServer(requestListener);
+server.listen(8080);
+
+async function getTitleJson(){
+  let browser = await puppeteer.launch({headless: true});
+  let page = await browser.newPage();
+  await page.goto('https://www.youtube.com/c/OfficialDuckStudios/videos/');
+  await page.click('button[aria-label="Agree to the use of cookies and other data for the purposes described"]');
+  await page.waitForNavigation();
+  const data = await page.evaluate(async () => {
+    return await new Promise((resolve, reject) =>{
+      try{
+        let scrollCheck = [];
+        let scrollCheckLast = [];
+        let intervaller = setInterval(() => {
+          scrollCheck.push(window.scrollY);
+          scrollCheckLast = scrollCheck.slice(-6);
+          if(scrollCheckLast.every( (val, i, arr) => val === arr[0] ) && scrollCheckLast[0] != 0){
+            letdata = Array.from(document.querySelectorAll('#video-title'), element => [element.textContent, element.href]);
+            clearInterval(intervaller);
+            resolve(letdata);
+          }
+            window.scrollBy(0, 1920);
+        }, 500);
+
+      }
+      catch(err){
+          console.log(err);
+          reject(err.toString());
+      }
     });
-    let dataArray = [];
-    data.forEach(element => {
-        const object = {
-            "title": element[0],
-            "link": element[1]
-        }
-        dataArray.push(object);
-    });
-    const jsonData = JSON.stringify(dataArray, null, '\t')
-    fs.writeFileSync('duck.json', jsonData);
-    await browser.close();
-
-})();
+  });
+  let dataArray = [];
+  data.forEach(element => {
+    const object = {
+      "title": element[0],
+      "link": element[1]
+    }
+    dataArray.push(object);
+  });
+  const jsonData = JSON.stringify(dataArray, null, '\t')
+  fs.writeFileSync('duck.json', jsonData);
+  const date = new Date(Date.now())
+  console.log(`Scraping succeeded at ${date.toUTCString()}`);
+  await browser.close();
+  setTimeout(getTitleJson, 600000);
+}
 
 
--- js/videolist.js
+++ js/videolist.js
@@ -1,6 +1,29 @@
 const videoBox = document.getElementById("video-box");
-let data;
-$.getJSON("js/duck.json", function (json){
+const online = false;
+const url = "http://localhost:8080";
+
+const Http = new XMLHttpRequest();
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  if(Http.readyState === XMLHttpRequest.DONE){
+    const status = Http.status;
+    console.log(status);
+    if(status >= 200 && status < 400){
+      console.log("http request succeeded");
+      jsonToHTML(JSON.parse(Http.responseText));
+    }
+    else{
+      console.log("Something went wrong, status code: " + status + "falling back to offline version");
+      $.getJSON("js/duck.json", function (json){
+        jsonToHTML(json);
+      });
+    }
+  }
+}
+
+function jsonToHTML(json){
   json.forEach((val) =>{
     const div = document.createElement("div");
     const a = document.createElement("a");
@@ -11,4 +34,4 @@ $.getJSON("js/duck.json", function (json){
     div.appendChild(a);
     videoBox.appendChild(div);
   });
-});
+}
